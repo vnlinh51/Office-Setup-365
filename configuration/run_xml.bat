d8fbbd9428e96f1f159caa50f11247744fbe43e3
vnlinh51 <vnlinh51@gmail.com>
2021-08-30 19:55:01 +0700

    Configuration

---
+++ configuration/run_xml.bat
@@ -0,0 +1,35 @@
+@echo off
+title Setup-Office-365
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo  Run CMD as Administrator...
+    goto goUAC 
+) else (
+ goto goADMIN )
+
+:goUAC
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:goADMIN
+pushd "%CD%"
+CD /D "%~dp0"
+cls
+
+echo.
+echo Installing Office ...
+
+setup.exe  /configure configuration.xml
+cls
+
+echo.
+echo Successful Installation!
+echo.
+
+timeout 4
+exit
+
