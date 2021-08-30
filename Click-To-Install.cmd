1cf7b3b11b3f8cf5d188043742d201a2da357b12
vnlinh51 <84014726+vnlinh51@users.noreply.github.com>
2021-08-30 19:42:59 +0700

    Configuration
    Configuration options using the Office Deployment Tool

---
+++ Click-To-Install.cmd
@@ -0,0 +1,26 @@
+@echo off
+title SETUP OFFICE 365 WORD - PP - EXCEL
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo  Run CMD as Administrator...
+    goto goUAC 
+) else (
+ goto goADMIN )
+
+:goUAC
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:goADMIN
+pushd "%CD%"
+CD /D "%~dp0"
+cls
+
+start configuration\run_xml.bat
+cls
+
+exit
